504fb72b6882a3cfb1ba3773fe640f92f1a387a8
tWangchen <tshering.wangchen@gmail.com>
2025-01-28 21:02:02 +1100

    Minor adjustments.

--- merge_pdfs.py
+++ merge_pdfs.py
@@ -4,15 +4,18 @@ from pypdf import PdfWriter
 # List the pdfs to merge
 INPUT_PDF_LIST = ["1.pdf", "2.pdf"]
 
+# Merged output pdf filename
+OUTPUT_PDF = "merged.pdf"
+
 merger = PdfWriter()
 
 
-def main(input_pdf_list) -> None:
+def main(input_pdf_list, output_pdf) -> None:
     for pdf in input_pdf_list:
         merger.append(pdf)
-    merger.write("merged.pdf")
+    merger.write(f"{OUTPUT_PDF}")
     merger.close()
 
 
 if __name__ == "__main__":
-    main(input_pdf_list=INPUT_PDF_LIST)
+    main(input_pdf_list=INPUT_PDF_LIST, output_pdf=OUTPUT_PDF)
